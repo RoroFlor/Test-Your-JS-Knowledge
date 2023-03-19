9ed2507ecba5a80aff6d9039d9459c8fb7301ff9
Humberto Alfaro <humbertoalfaro@Humbertos-MBP.lan1>
2023-03-19 19:16:18 -0400

    created and styledquiz start page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let startQuizBtn = document.querySelector('.startBtn')
+let h1Replacement = document.querySelector('.challengeQuestion')
+let multipleChoice = document.querySelector('.answers')
+
+function questions() {
+h1Replacement.textContent = 'Commonly used data types DO Not Include:'
+// multipleChoice.textContent = 
+}
+
+
+startQuizBtn.addEventListener('click', questions)
